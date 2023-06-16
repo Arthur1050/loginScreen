098093cab3ceb075749e79d09dd1fcdfe5885703
Arthur Tosta <tostamatias@gmail.com>
2023-06-15 23:52:31 -0300

    wip: ainda na implementação do slide em InfoLogin.tsx.

--- src/components/SlideComents.tsx
+++ src/components/SlideComents.tsx
@@ -1,16 +1,47 @@
+import './SlideComents.css';
+
+const comments = [
+    {
+        user: 'Maria_Silva123',
+        comment: 'Estou maravilhada com a variedade de produtos disponíveis neste site! Como uma empreendedora do dropshipping, é essencial ter acesso a fornecedores confiáveis e produtos de qualidade. Este site superou todas as minhas expectativas.'
+    },
+    {
+        user: 'Ana_Santos89',
+        comment: 'Este site é perfeito para as empreendedoras que desejam fazer dropshipping. A variedade de produtos é impressionante e sempre encontro algo para atender às demandas dos meus clientes.'
+    },
+    {
+        user: 'Juliana_Oliveira',
+        comment: 'O suporte ao cliente também é excelente e me ajudou em todas as minhas dúvidas. Recomendo este site de olhos fechados!'
+    },
+    {
+        user: 'Luiza_Rodrigues22',
+        comment: 'Como uma mulher empreendedora, encontrar um site de dropshipping confiável e com produtos de qualidade é essencial. Este site superou todas as minhas expectativas. Os produtos são incríveis e a plataforma é fácil de usar.'
+    },
+    {
+        user: 'Beatriz_Costa',
+        comment: 'Estou encantada com a qualidade dos produtos deste site! Como uma empreendedora do dropshipping, é essencial oferecer produtos de alto padrão aos meus clientes. Este site me proporciona exatamente isso.'
+    },
+    {
+        user: 'Carolina_Machado',
+        comment: 'Este site é a minha escolha número um para fazer dropshipping! A variedade de produtos é incrível e sempre encontro algo novo e exclusivo para oferecer aos meus clientes. A entrega é rápida e confiável, e a equipe de suporte é excepcional.'
+    }
+]
+
 export default function SlideComents() {
     return (
-        <div className="p-1 flex-auto">
-            <div className="flex gap-1 h-full text-left">
-                <div className="p-2 w-3/4 border border-violet-500/50 text-gray-100 bg-violet-500/70 rounded-lg">
-                    <div>
-                        <img className="mr-2 w-7 h-7 rounded-full inline-block" src="/src/assets/perfis/perfil1.jpg" alt="" />
-                        <span className="text-sm font-semibold">Maria_Silva123</span>
+        <div className="py-1 flex-auto">
+            <div className="carroflex flex gap-1 h-full text-left">
+                {comments.map((comment, i) => (
+                    <div className="p-2 min-w-[75%] border border-violet-500/50 text-gray-100 bg-violet-500/70 rounded-lg">
+                        <div>
+                            <img className="mr-2 w-7 h-7 rounded-full inline-block" src={'/src/assets/perfis/perfil'+(i+1)+'.jpg'} alt="" />
+                            <span className="text-sm font-semibold">{comment.user}</span>
+                        </div>
+                        <p className="text-xs indent-3 mt-2 h-20 overflow-hidden">
+                            {comment.comment}
+                        </p>
                     </div>
-                    <p className="text-xs indent-3 mt-2">
-                        Estou maravilhada com a variedade de produtos disponíveis neste site! Como uma empreendedora do dropshipping, é essencial ter acesso a fornecedores confiáveis e produtos de qualidade. Este site superou todas as minhas expectativas.
-                    </p>
-                </div>
+                ))}
             </div>
         </div>
     )
